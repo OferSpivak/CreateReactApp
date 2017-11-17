09145d59f08dc4cc55e2d0fb087c9d9183029b87
OferSpivak <ofer.spivak@gmail.com>
2017-11-17 15:41:07 +0200

    first commint

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-function App() {
-  return React.createElement('div', null, [
-  React.createElement('h1', null, 'Hello'),
-  React.createElement('h2', null, 'There')
-  ]);
- } 
-
 ReactDOM.render(
-  App(),
+  React.createElement('div', null, 'Hello world!'),
   document.getElementById('root')
 );
